5d1d3718015987e3ae946ed1028deea075992a5e
Harsh <harsh.singh0310@gmail.com>
2024-03-16 22:19:45 +0530

    added blog section

---
+++ src/components/Blog/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from "./blog.module.css";
+
+export const blog = () => {
+  return (
+    <div className={styles.container}>
+
+        
+    </div>
+  )
+}
