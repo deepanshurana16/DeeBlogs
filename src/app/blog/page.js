c85070140e5314bbae267bfc0767c08882ef0338
Harsh <harsh.singh0310@gmail.com>
2024-03-17 22:24:07 +0530

    added folder struture

---
+++ src/app/blog/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Blog from '@/components/Blog/blog';
+
+ const page = () => {
+  return (
+<Blog></Blog>
+  )
+}
+export  default page;
--- src/components/Blog/blog.js
+++ src/components/Blog/blog.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import styles from "./blog.module.css";
 
-export const blog = () => {
+const Blog = () => {
   return (
     <div className={styles.container}>
-
+<p>Blog</p>
         
     </div>
   )
 }
+export default Blog;
