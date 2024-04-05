e5b8a87cd5d84261cb1a79263c26bdd43acbd842
Harsh <harsh.singh0310@gmail.com>
2024-04-05 16:59:51 +0530

    made footer and improved text-editor

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,16 +1,17 @@
 import React from 'react'
 import style from "./footer.module.css"
+import Link from 'next/link'
 export const Footer = () => {
   return (
     <div className={style.container}>
-        <hr></hr>
-        <div>made by Harsh Singh</div>
-        <div className={style.links}>
-            <div className={style.items}>Github</div>
-            <div className={style.items}>Linkedin</div>
-            <div className={style.items}>Leetcode</div>
-            <div className={style.items}>Codeforces</div>
-        </div>
+        <hr className={style.line}></hr>
+        <div className={style.footext}>made by Harsh Singh</div>
+        <li className={style.links}>
+            <ul className={style.items}><Link href="https://github.com/lives-in-virgosupercluster" className={style.linkitems}>Github</Link></ul>
+            <ul className={style.items}><Link href="https://www.linkedin.com/in/harshsingh0310/" className={style.linkitems}>Linkedin</Link></ul>
+            <ul className={style.items}><Link href="https://leetcode.com/Solar_Nebula/" className={style.linkitems}>Leetcode</Link></ul>
+            <ul className={style.items}><Link href="https://codeforces.com/profile/idonthatephy" className={style.linkitems}>Codeforces</Link></ul>
+        </li>
 
     </div>
   )
--- src/components/write/page.js
+++ src/components/write/page.js
@@ -1,13 +1,82 @@
 "use client"
-import React, { useState } from 'react'
-import styles from "./write.module.css"
-import Image from 'next/image'
-import ReactQuill from 'react-quill'
+// import React, { useState } from 'react'
+// import styles from "./write.module.css"
+// import Image from 'next/image'
+// import ReactQuill from 'react-quill'
+// import 'react-quill/dist/quill.bubble.css';
+// import { useRouter } from 'next/navigation';
+
+// const Writepage = () => {
+//     const router=useRouter();
+//     const [open, setOpen] = useState(false);
+//     const [value, setValue] = useState("");
+//     const [title, setTitle] = useState("");
+
+//     const handlePublish = async () => {
+//         try {
+//             console.log(value);
+//             console.log(title);
+//             const response = await fetch('/api/write', {
+//                 method: 'POST',
+//                 headers: {
+//                     'Content-Type': 'application/json',
+//                 },
+//                 body: JSON.stringify({
+//                     title: title,
+//                     content: value,
+//                     tag: "reactjs" 
+//                 }),
+//             });
+
+//             if (!response.ok) {
+//                 throw new Error('no network response');
+//             }
+
+            
+//             console.log('Post successfully published');
+//         } catch (error) {
+//             console.error('Error publishing post:', error);
+            
+//         }
+//     };
+
+//     return (
+//         <div className={styles.container}>
+//             <input type="text" placeholder="Title" className={styles.input} onChange={(e) => setTitle(e.target.value)} />
+//             <div className={styles.editor}>
+//                 <button className={styles.button} onClick={() => setOpen(!open)}>
+//                     <Image src="/images/plus.png" alt="" width={16} height={16} />
+//                 </button>
+//                 {open && (
+//                     <div className={styles.add}>
+//                         <button className={styles.addbutton}>
+//                             <Image src="/images/image.png" alt="" width={16} height={16} />
+//                         </button>
+//                         <button className={styles.addbutton}>
+//                             <Image src="/images/external.png" alt="" width={16} height={16} />
+//                         </button>
+//                         <button className={styles.addbutton}>
+//                             <Image src="/images/video.png" alt="" width={16} height={16} />
+//                         </button>
+//                     </div>
+//                 )}
+//                 <ReactQuill theme="bubble" value={value} onChange={setValue} placeholder="Tell Your Story" className={styles.TextArea} />
+//             </div>
+//             <button className={styles.publish} onClick={handlePublish}>Publish</button>
+//         </div>
+//     );
+// };
+
+// export default Writepage;
+import React, { useState } from 'react';
+import styles from "./write.module.css";
+import Image from 'next/image';
+import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.bubble.css';
 import { useRouter } from 'next/navigation';
 
 const Writepage = () => {
-    const router=useRouter();
+    const router = useRouter();
     const [open, setOpen] = useState(false);
     const [value, setValue] = useState("");
     const [title, setTitle] = useState("");
@@ -32,12 +101,20 @@ const Writepage = () => {
                 throw new Error('no network response');
             }
 
-            
             console.log('Post successfully published');
         } catch (error) {
             console.error('Error publishing post:', error);
-            
         }
+    };
+
+    const modules = {
+        toolbar: [
+            [{ 'header': '1' }, { 'header': '2' }, { 'font': [] }],
+            ['bold', 'italic', 'underline', 'strike'],
+            [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+            ['link', 'image', 'video'],
+            ['clean']
+        ],
     };
 
     return (
@@ -60,7 +137,14 @@ const Writepage = () => {
                         </button>
                     </div>
                 )}
-                <ReactQuill theme="bubble" value={value} onChange={setValue} placeholder="Tell Your Story" className={styles.TextArea} />
+                <ReactQuill 
+                    theme="bubble" 
+                    value={value} 
+                    onChange={setValue} 
+                    placeholder="Tell Your Story" 
+                    className={styles.TextArea} 
+                    modules={modules} // Include the modules object here
+                />
             </div>
             <button className={styles.publish} onClick={handlePublish}>Publish</button>
         </div>
@@ -68,3 +152,4 @@ const Writepage = () => {
 };
 
 export default Writepage;
+
